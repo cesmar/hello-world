920f163d173676c055a87ba37033dc3a9a0dce58
Carlos Espino Maraví <espinomaravic@gmail.com>
2016-06-21 15:44:05 -0500

    Changes for filtering and order by

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -6,14 +6,24 @@ angular.
 			this.phones = [
 				{
 					name:'Nexus S',
-					snippet: 'Fast just got faster with Nexus S.'
+					snippet: 'Fast just got faster with Nexus S.',
+					age: 1
 				}, {
 					name:'Motorola XOOM™ with Wi-Fi',
-					snippet: 'The Next, Next Generation tablet.'
+					snippet: 'The Next, Next Generation tablet.',
+					age: 2
 				}, {
 					name:'Motorola XOOM',
-					snippet: 'The Next, Next Generation tablet.'
+					snippet: 'The Next, Next Generation tablet.',
+					age: 3
+				}, {
+					name:'LG G5',
+					snippet: 'The best one. All in one',
+					age: 4
 				}
 			];
+
+			this.orderProp = 'age';
 		}
 	});
+
